108621c6714367e21fc1cbf5d6df23ff39f45051
hyuk <wold2180@gmail.com>
2025-09-11 18:45:58 +0900

    update:3003까지 풀었음

--- Java/3003.java
+++ Java/3003.java
@@ -10,8 +10,13 @@ public class Main {
         for(int i=0; i<pieces.length; i++){
             int piece = Integer.parseInt(pieces[i]);
             result.append(pieces_count[i] - piece);
+            if(i == pieces_count.length - 1) {
+                result.append(" ");
+            } 
         }
         System.out.println(result);
         br.close();
     }
 }
+// StringJoiner 방법도 있음.
+// 특정한 문자를 미리 넣을 수도 있는 듯
